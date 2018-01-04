ca8beacb2081085a58665e674da50f82d249e34e
stalary <452024236@qq.com>
2018-01-04 17:22:21 +0800

    MOD:隐藏页面接口

--- src/main/java/com/stalary/controller/IndexController.java
+++ src/main/java/com/stalary/controller/IndexController.java
@@ -8,6 +8,7 @@ package com.stalary.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import springfox.documentation.annotations.ApiIgnore;
 
 /**
  * IndexController
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @since 02/01/2018
  */
 @Controller
+@ApiIgnore
 public class IndexController {
 
     @RequestMapping(value = "/")
